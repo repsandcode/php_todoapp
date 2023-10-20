ab1f0de392632b42dce02450d9d6d2734a1ebcb3
repsandcode <albertjonathan.dev@gmail.com>
2023-10-21 02:47:16 +0800

    arrange php and html code blocks

--- index.php
+++ index.php
@@ -6,6 +6,43 @@ ini_set('display_errors', 1);
 
 // connecting with another php file - database.php
 include("database.php");
+
+$message = '';
+
+// run if user interacted with the form and hit the submit button
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // extract the data from the input text named "task"
+    // saved to variable -> $task
+    $task = $_POST["task"];
+
+    // sql syntax to insert $task to the table named "tasks"
+    // saved to variable -> $sql
+    $sql = "INSERT INTO tasks (task) VALUES ('$task')";
+
+    // get database connection and insert our add-task query
+    if ($conn->query($sql) === TRUE) {
+        // if that is true, then show code below
+        $message = "\n\nNew task added successfully!";
+    } else {
+        // else, run this code and show error
+        $message = "\n\nError: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+
+if (isset($_GET['task_id'])) {
+    $taskId = $_GET['task_id'];
+
+    $taskStatus = "SELECT completed FROM tasks WHERE id = $taskId";
+    $checkComplete = "UPDATE tasks SET completed = 1 WHERE id = $taskId";
+
+    if ($conn->query($checkComplete) === TRUE) {
+        $message = "Task marked as completed!";
+    } else {
+        $message = "Error updating record: " . $conn->error;
+    }
+}
+
 ?>
 
 <!-- start of the HTML -->
@@ -19,43 +56,6 @@ include("database.php");
 </head>
 
 <body>
-    <!-- PHP code inside the body -->
-    <?php
-    // run if user interacted with the form and hit the submit button
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // extract the data from the input text named "task"
-        // saved to variable -> $task
-        $task = $_POST["task"];
-
-        // sql syntax to insert $task to the table named "tasks"
-        // saved to variable -> $sql
-        $sql = "INSERT INTO tasks (task) VALUES ('$task')";
-
-        // get database connection and insert our add-task query
-        if ($conn->query($sql) === TRUE) {
-            // if that is true, then show code below
-            echo "\n\nNew task added successfully!";
-        } else {
-            // else, run this code and show error
-            echo "\n\nError: " . $sql . "<br>" . $conn->error;
-        }
-    }
-
-
-    if (isset($_GET['task_id'])) {
-        $taskId = $_GET['task_id'];
-
-        $taskStatus = "SELECT completed FROM tasks WHERE id = $taskId";
-        $checkComplete = "UPDATE tasks SET completed = 1 WHERE id = $taskId";
-
-        if ($conn->query($checkComplete) === TRUE) {
-            echo "Task marked as completed!";
-        } else {
-            echo "Error updating record: " . $conn->error;
-        }
-    }
-
-    ?>
 
     <h2>Todo List</h2>
     <form method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
@@ -64,14 +64,13 @@ include("database.php");
         <button type="submit">Add Task</button>
     </form>
 
+    <!-- display messages -->
+    <p><?php $message ?></p>
 
     <?php
     $sql = "SELECT * FROM tasks";
     $result = $conn->query($sql);
-    ?>
 
-    <h5>Todos</h5>
-    <?php
     if ($result->num_rows > 0) {
         echo "<ul>";
         while ($row = $result->fetch_assoc()) {
@@ -84,9 +83,7 @@ include("database.php");
         }
         echo "</ul>";
     }
-    ?>
 
-    <?php
     $conn->close();
     ?>
 </body>
